e30351e9c6ba371878ada1d3f94e825b506a4baa
montacie <isha@d219-tyratron.paris-sorbonne.fr>
2016-10-20 11:09:35 +0200

    correction du 1er exercice de l'atelier4

--- Montacié Programmation Objet et Groupware/src/atelier04/Controle1.java
+++ Montacié Programmation Objet et Groupware/src/atelier04/Controle1.java
@@ -2,12 +2,23 @@ package atelier04;
 
 public class Controle1 {
 	public static void main(String[] args) {
-	int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8;
-	
-	
-	
-	System.out.println(p4);
-	;
+		int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8;
+
+		if (p1 > p2) {
+			if (p1 > p3)
+				p4 = p1;
+			else
+				p4 = p3;
+		} 
+		// p2 >= p1
+		else {
+			if (p2 > p3)
+				p4 = p2;
+			else
+				p4 = p3;
+		}
+
+		System.out.println(p4);
+
 	}
 }
-
