1defa48c16805349eb08d06170f48687f4044485
montacie <isha@d219-tyratron.paris-sorbonne.fr>
2016-10-20 11:52:55 +0200

    correction de l'exercice 2

--- Montacié Programmation Objet et Groupware/src/atelier04/Controle2.java
+++ Montacié Programmation Objet et Groupware/src/atelier04/Controle2.java
@@ -1,6 +1,7 @@
 package atelier04;
 
-/** Recherche du minimum de 3 variables
+/**
+ * Recherche du minimum de 3 variables
  * 
  * @author montacie
  *
@@ -10,18 +11,18 @@ public class Controle2 {
 	public static void main(String[] args) {
 		int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8;
 
-		if (p1 > p2) {
-			if (p1 > p3)
+		if (p1 < p2) {
+			if (p1 < p3) {
 				p4 = p1;
-			else
+			} else {
 				p4 = p3;
-		} 
-		// p2 >= p1
-		else {
-			if (p2 > p3)
+			}
+		} else {
+			if (p2 < p3) {
 				p4 = p2;
-			else
+			} else {
 				p4 = p3;
+			}
 		}
 
 		System.out.println(p4);
